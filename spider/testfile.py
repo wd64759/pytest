dde4f9f670baf58cecf0fdad8a0bdda948373300
wei Tony <wei Tony>
2016-11-27 23:41:20 +0800

    add more files on trace

--- spider/testfile.py
+++ spider/testfile.py
@@ -1,13 +1,25 @@
 import os
 import requests
+import time
 from bs4 import BeautifulSoup
 
 f = open('config/stock_ref.props','r')
 d = dict((x.split('=')[0:2]) for x in f.readlines())
-print(d['stock_available_total'])
+# print(d['stock_available_total'])
 
-r = requests.get('https://gupiao.baidu.com/stock/sh601857.html')
+t1 = time.time()
+r = requests.get('https://gupiao.baidu.com/stock/sh510130.html')
+# r.close()
+
 r.encoding = 'utf-8'
 soup = BeautifulSoup(r.text, 'html.parser')
-p_elem = soup.select_one('#app-wrap > div.stock-info > div > div.price.s-up > span:nth-of-type(2)')
-print(p_elem)
+# p_elem = soup.select_one('#app-wrap > div.stock-info > div > div.price.s-up > span:nth-of-type(2)')
+# p_elem = soup.select_one("dd.s-up")
+# p_elem = soup.find('dt', text='最高')
+p_elem = soup.select_one('#app-wrap > div.stock-info > div > div.bets-content')
+p_elem = p_elem.find('dt', text='最高')
+n = p_elem.find_next_sibling('dd')
+t2 = time.time()
+# print(n, ' time cost:', str(t2 - t1) )
+
+
---
+++ spider/util.py
@@ -0,0 +1,16 @@
+import re
+import pprint
+
+
+def load_stocks():
+    stock_list = []
+    with open('config/stock_sh.list',encoding='utf-8') as f:
+        line = f.readline()
+        while line:
+            p = re.compile('(.*)\((\d+)\)',re.U)
+            m = re.match(p, line)
+            if m is not None:
+                stock_list.append(m.groups()[::-1])
+            line = f.readline()
+    return stock_list
+
